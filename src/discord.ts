5fd8174a1400ee67855611a63c493575b5dbdc67
jython234 <jython234@users.noreply.github.com>
2018-03-07 21:45:22 -0600

    Auto-generate discord-bridged rooms when attempting to join using cool new system!

--- src/discord.ts
+++ src/discord.ts
@@ -1,4 +1,5 @@
 import * as Discord from "discord.js";
+import { RemoteRoom } from "matrix-appservice-bridge";
 
 import { DiscordMatrixBridge } from "./main";
 
@@ -22,8 +23,7 @@ export class DiscordBot {
 
     private onReady() {
         console.log("Connected to Discord.");
-        let intent = self.bridge.matrixAppservice.matrixBridge.getIntent();
-        
+
         self.client.guilds.forEach((guild) => {
             guild.channels.forEach((channel) => {
                 if(channel instanceof Discord.TextChannel) {
@@ -32,6 +32,9 @@ export class DiscordBot {
                     let canInvite = permissions.has(Discord.Permissions.FLAGS.CREATE_INSTANT_INVITE);
 
                     if(canAccess) {
+                        self.tryInsertNewRemoteRoom(self, channel.id.substr(channel.id.length - 4), guild, channel, canInvite, canAccess);
+
+                        /*console.log("Creating room for #" + channel.name);
                         intent.createRoom({
                             createAsClient: false,
                             options: {
@@ -39,12 +42,48 @@ export class DiscordBot {
                                 name: channel.name,
                                 topic: channel.topic,
                                 visibility: canInvite ? "public" : "private",
-                                room_alias_name: "discord_#" + channel.name
+                                room_alias_name: "discord_!" + channel.id
                             }
                         });
+                        intent.leave()*/
                     }
                 }
             });
+        });
+    }
+
+    private tryInsertNewRemoteRoom(self: DiscordBot, roomNumber, guild: Discord.Guild, channel: Discord.TextChannel, canInvite: boolean, canAccess: boolean) {
+        let intent = self.bridge.matrixAppservice.matrixBridge.getIntent();
+        let roomStore = self.bridge.matrixAppservice.matrixBridge.getRoomStore();
+
+        let roomId = channel.name + ";" + roomNumber;
+        roomStore.getEntryById(roomId).then((entry) => {
+            console.log("Entry for (" + roomId + "): " + entry);
+            if(entry == null) {
+                console.log("Creating entry and inserting.");
+                let room = new RemoteRoom(roomId);
+                room.set("type", "discord-text");
+                room.set("guild", guild.id);
+                room.set("channel", channel.id);
+                roomStore.upsertEntry({
+                    id: roomId,
+                    matrix: null,
+                    remote: room,
+                    data: {
+                        matrixPreset: canInvite ? "public_chat" : "private_chat",
+                        name: channel.name,
+                        guild: guild.name,
+                        topic: channel.topic,
+                        visibility: canInvite ? "public" : "private"
+                    }
+                });
+            } else {
+                // Check if it matches our guild and channel
+                if(entry.remote.get("guild") == guild.id && entry.remote.get("channel") == channel.id) return;
+
+                // Create a new room as it didn't match our guild and channel, so it's a different discord room we found.
+                self.tryInsertNewRemoteRoom(self, roomNumber - 1, guild, channel, canInvite, canAccess);
+            }
         });
     }
 }
--- src/matrix.ts
+++ src/matrix.ts
@@ -4,7 +4,7 @@ import { DiscordMatrixBridge } from "./main";
 
 import { join } from "path";
 
-const appserviceUserPart = "_appservice-discord";
+const appserviceUserPart = "appservice-discord";
 
 var self;
 
@@ -35,7 +35,8 @@ export class MatrixAppservice {
         reg.setHomeserverToken(AppServiceRegistration.generateToken());
         reg.setAppServiceToken(AppServiceRegistration.generateToken());
         reg.setSenderLocalpart(appserviceUserPart);
-        reg.addRegexPattern("users", "@discord_.*", true);
+        reg.addRegexPattern("users", "@!discord_.*", true);
+        reg.addRegexPattern("aliases", "#!discord_.*", true);
         callback(reg);
     }
 
@@ -47,6 +48,7 @@ export class MatrixAppservice {
 
             controller: {
                 onUserQuery: self.onUserQuery,
+                onAliasQuery: self.onAliasQuery,
                 onEvent: self.onEvent
             }
         });
@@ -61,6 +63,39 @@ export class MatrixAppservice {
 
     private onUserQuery(queriedUser): object {
         return {};
+    }
+
+    private onAliasQuery(alias, aliasLocalpart): Promise<object> {
+        let discordRoom = aliasLocalpart.split("_")[1].replace("#", "");
+        console.log("Processing alias for " + alias + " (" + aliasLocalpart + ")" + ", discord room is: " + discordRoom);
+
+        let intent = self.matrixBridge.getIntent();
+        let roomStore = self.matrixBridge.getRoomStore();
+
+        return new Promise((resolve, reject) => {
+            roomStore.getEntriesByRemoteId(discordRoom).then((values) => {
+                if(values == null || values.length == 0) {
+                    console.error("Failed to find discordRoom matching: " + discordRoom);
+                    reject();
+                    return;
+                }
+
+                let value = values[0];
+
+                console.log(value);
+
+                resolve({
+                    creationOpts: {
+                        room_alias_name: aliasLocalpart,
+                        name: "#" + value.data.name + " (" + value.data.guild + ") [Discord]",
+                        topic: value.data.topic,
+                        visibility: value.data.visibility,
+                        preset: value.data.matrixPreset
+                    },
+                    remote: value
+                });
+            });
+        });
     }
 
     private onEvent(request, context) {
